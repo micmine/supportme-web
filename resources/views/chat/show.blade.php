43c464324afe7ef6d96bd73962875101ae49cde3
micmine <micmine4@gmail.com>
2020-03-12 18:09:35 +0100

    Add style structur

--- resources/views/chat/show.blade.php
+++ resources/views/chat/show.blade.php
@@ -7,27 +7,28 @@
 @section('content')
 
 <h1 class="">{{ $chat->name }}</h1>
-
-<div class="chat-container">
-    @forelse ($messages as $message)
-        <div class="chat-message {{ Auth::user()->id == $message->user_id ? 'chat-message-right' : 'chat-message-left' }}">
-            <a href="">{{ App\User::find($message->user_id)->email }}</a>
-            <p>{{ $message->message }}</p>
-        </div>
-    @empty
-        <div class="chat-message center">
-            <p>Here are no messages</p>
-        </div>
-    @endforelse
-</div>
-
-<form action="{{ route('chatmessage.store') }}" method="POST" class="chat-form">
-    @csrf
-    <div class="row">
-        <input type="hidden" name="chat_id" value="{{ $chat->id}}">
-        <input type="text" class="form-control col-10 pr-1" name="message" placeholder="message">
-        <button type="submit" class=" col-2 btn btn-primary">send</button>
+<div class="chat-page">
+    <div class="chat-container chat-push">
+        @forelse ($messages as $message)
+            <div class="chat-message {{ Auth::user()->id == $message->user_id ? 'chat-message-right' : 'chat-message-left' }}">
+                <a href="">{{ App\User::find($message->user_id)->email }}</a>
+                <p>{{ $message->message }}</p>
+            </div>
+        @empty
+            <div class="chat-message center">
+                <p>Here are no messages</p>
+            </div>
+        @endforelse
     </div>
-</form>
+
+    <form action="{{ route('chatmessage.store') }}" method="POST" class="chat-form">
+        @csrf
+        <div class="row">
+            <input type="hidden" name="chat_id" value="{{ $chat->id}}">
+            <input type="text" class="form-control col-10 pr-1" name="message" placeholder="message">
+            <button type="submit" class=" col-2 btn btn-primary">send</button>
+        </div>
+    </form>
+</div>
 
 @endsection
