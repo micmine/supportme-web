206ae570ab00596cb5763ad45bf3ce60e138189f
micmine <micmine4@gmail.com>
2020-03-25 13:49:04 +0100

    fix isUser permission check

--- app/User.php
+++ app/User.php
@@ -8,41 +8,43 @@ use Illuminate\Notifications\Notifiable;
 
 class User extends Authenticatable
 {
-    use Notifiable;
+	use Notifiable;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array
-     */
-    protected $fillable = [
-        'name', 'email', 'password',
-    ];
+	/**
+	 * The attributes that are mass assignable.
+	 *
+	 * @var array
+	 */
+	protected $fillable = [
+		'name', 'email', 'password',
+	];
 
-    /**
-     * The attributes that should be hidden for arrays.
-     *
-     * @var array
-     */
-    protected $hidden = [
-        'password', 'remember_token',
-    ];
+	/**
+	 * The attributes that should be hidden for arrays.
+	 *
+	 * @var array
+	 */
+	protected $hidden = [
+		'password', 'remember_token',
+	];
 
-    /**
-     * The attributes that should be cast to native types.
-     *
-     * @var array
-     */
-    protected $casts = [
-        'email_verified_at' => 'datetime',
-    ];
+	/**
+	 * The attributes that should be cast to native types.
+	 *
+	 * @var array
+	 */
+	protected $casts = [
+		'email_verified_at' => 'datetime',
+	];
 
-    public function groups() {
-        return $this->belongsToMany(Group::class);
-    }
+	public function groups()
+	{
+		return $this->belongsToMany(Group::class);
+	}
 
-    public function chats() {
-        /*
+	public function chats()
+	{
+		/*
         $chats = [];
 
         foreach (Auth::user()->chats()->get() as $chat) {
@@ -58,10 +60,11 @@ class User extends Authenticatable
         
         return collection(array_unique($chats));
         */
-        return $this->belongsToMany('App\Chat', 'entity_chat', 'user_id', 'id');
-    }
+		return $this->belongsToMany('App\Chat', 'entity_chat', 'user_id', 'id');
+	}
 
-    public function isUser() {
-        return !$this->groups()->where('name', 'admin')->exists();
-    }
+	public function isUser()
+	{
+		return !$this->groups()->where('name', 'team')->exists();
+	}
 }
