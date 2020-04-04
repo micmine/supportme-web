2b075a2f24d3e099f4c4095a2fe8abc6219f457b
micmine <micmine4@gmail.com>
2020-04-04 22:57:58 +0200

    Add editor.sh

---
+++ editor.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+SESSION="supportme-web"
+SESSIONEXISTS=$(tmux list-sessions | grep $SESSION)
+
+if [ "$SESSIONEXISTS" = "" ]
+then
+	tmux new-session -d -s $SESSION
+
+	tmux new-window -t $SESSION:1 -n 'edit'
+	tmux send-keys -t 'edit' 'nvim' C-m
+
+	tmux new-window -t $SESSION:2 -n 'php'
+	tmux send-keys -t 'php' "php artisan serve" C-m
+
+	tmux new-window -t $SESSION:3 -n 'npm'
+	tmux send-keys -t 'npm' 'npm run watch-poll' "" C-m
+
+	tmux new-window -t $SESSION:4 -n 'tinker'
+	tmux send-keys -t 'tinker' "php artisan tinker" C-m
+
+	tmux new-window -t $SESSION:5 -n 'shell'
+	tmux send-keys -t 'shell' "bash" C-m
+
+fi
+
+tmux attach-session -t $SESSION:1
