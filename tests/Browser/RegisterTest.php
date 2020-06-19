57827d5c823becab8a6ecbdaf5465f3f39c0aeab
micmine <micmine4@gmail.com>
2020-06-19 12:24:44 +0200

    :heavy_check_mark: Fix Register test

--- tests/Browser/RegisterTest.php
+++ tests/Browser/RegisterTest.php
@@ -18,27 +18,12 @@ class RegisterTest extends DuskTestCase
 	 */
 	public function register()
 	{
-		$user = factory(User::class)->create([
-			'email' => 'taylor@laravel.com',
-			'password' => bcrypt('normal'),
-			'name' => 'taylor'
-		]);
-
-		$this->browse(function (Browser $browser) use ($user) {
+		$this->browse(function (Browser $browser) {
 			$browser->visit(new RegisterPage)
                 ->register("hans" ,"hans@gmail.com" , 'oiuhgiuerh33goweiruhgergo8743z87zhk')
                 ->screenshot("after_register")
-                ->waitForText('hans@gmail.com')
-                ->assertPathIs('/');
-		});
-	}
-
-	public function testloginRequired()
-	{
-		$this->browse(function ($browser) {
-			$browser->visit('/')
-				->waitForText('Login')
-				->assertPathIs('/login');
+                ->waitForText('hans')
+                ->assertPathIs('/chat');
 		});
 	}
 }
